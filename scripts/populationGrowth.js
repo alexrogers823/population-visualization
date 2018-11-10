23ae4cf6deb9cecde4995ad9aba5ef73da4bbac8
Alex Rogers <alex.rogers823@gmail.com>
2018-11-09 23:45:02 -0500

    separted components of building graph into async/await functions

--- scripts/populationGrowth.js
+++ scripts/populationGrowth.js
@@ -3,18 +3,16 @@
 //   .then(blob => blob.json())
 //   .then(data => {
 //     cities.push(...data);
-//     const states = [...new Set(cities.map(city => city.state))];
+    // const states = [...new Set(cities.map(city => city.state))];
 //     console.log(states);
 //   });
 // const cities = [];
 
 // Find out how to do this without pushing into a global variable
-async function loadData(variable) {
+async function loadData() {
   const data = await fetch('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json');
   const json = await data.json();
-  variable.push(...json);
-  // console.log(json);
-  // return json;
+  return json;
 }
 
 async function test() {
@@ -23,24 +21,38 @@ async function test() {
   console.log(cities);
 }
 
+async function main() {
+  const allCities = await loadData();
+  // console.log(allCities);
+  const states = [...new Set(allCities.map(city => city.state))]
+                  .sort();
+  // console.log(states[0]);
+  const cities = allCities.filter(city => city.state === states[6])
+                          .slice(0, 10);
+  // console.log(cities);
+  return cities;
+}
+
+
+
 // test();
 
 
 
 
 // This function to be used with the sort method on object-based arrays
-function compare(a, b) {
-  const stateA = a.state;
-  const stateB = b.state;
-
-  let comparison = 0;
-  if (stateA > stateB) {
-    comparison = 1;
-  } else if (stateA < stateB) {
-    comparison = -1;
-  }
-  return comparison;
-}
+// function compare(a, b) {
+//   const stateA = a.state;
+//   const stateB = b.state;
+//
+//   let comparison = 0;
+//   if (stateA > stateB) {
+//     comparison = 1;
+//   } else if (stateA < stateB) {
+//     comparison = -1;
+//   }
+//   return comparison;
+// }
 
 const [width, height] = [1000, 500];
 
@@ -54,61 +66,61 @@ const sampleData = [
     city: "New York",
     state: "New York",
     population: "2456933",
-  growth: "3.9%",
+  growth_from_2000_to_2013: "3.9%",
   },
   {
     city: "Chicago",
     state: "Illinois",
     population: "245934",
-  growth: "1.5%",
+  growth_from_2000_to_2013: "1.5%",
   },
   {
     city: "Los Angeles",
     state: "California",
     population: "1723745",
-  growth: "5.3%",
+  growth_from_2000_to_2013: "5.3%",
   },
   {
     city: "Minneapolis",
     state: "Minnesota",
     population: "456823",
-  growth: "4.8%",
+  growth_from_2000_to_2013: "4.8%",
   },
   {
     city: "Atlanta",
     state: "Georgia",
     population: "588923",
-  growth: "-2.7%",
+  growth_from_2000_to_2013: "-2.7%",
   },
   {
     city: "Houston",
     state: "Texas",
     population: "502984",
-  growth: "4.2%",
+  growth_from_2000_to_2013: "4.2%",
   },
   {
     city: "St. Louis",
     state: "Missouri",
     population: "233987",
-  growth: "7.9%",
+  growth_from_2000_to_2013: "7.9%",
   },
   {
     city: "San Francisco",
     state: "California",
     population: "998734",
-  growth: "0.6%",
+  growth_from_2000_to_2013: "0.6%",
   },
   {
     city: "Seattle",
     state: "Washington",
     population: "492834",
-  growth: "-1.3%",
+  growth_from_2000_to_2013: "-1.3%",
   },
   {
     city: "Miami",
     state: "Florida",
     population: "703958",
-  growth: "4.0%"
+  growth_from_2000_to_2013: "4.0%"
   }
 ];
 
@@ -143,68 +155,150 @@ const sampleData = [
 
 // Setting axis range of current growth dataset
 function growthRange(data) {
-  const min = Math.floor(Math.min(...data.map(n => parseFloat(n.growth))));
-  const max = Math.ceil(Math.max(...data.map(n => parseFloat(n.growth))));
+  const min = Math.floor(Math.min(...data.map(n => parseFloat(n.growth_from_2000_to_2013))));
+  const max = Math.ceil(Math.max(...data.map(n => parseFloat(n.growth_from_2000_to_2013))));
   return [min, max];
 }
 
-const [growthMin, growthMax] = growthRange(sampleData);
+async function makeAxis() {
+  const data = await main();
+  console.log(data);
+  const [growthMin, growthMax] = growthRange(data);
 
-// x-axis
-const cityScale = d3.scaleLinear()
-  .domain([1, 10])
+  // x-axis
+  const cityScale = d3.scaleLinear()
+  .domain([1, data.length])
   .range([50, width-50]);
 
-const cityAxis = d3.axisBottom(cityScale)
+  const cityAxis = d3.axisBottom(cityScale)
   .ticks(10)
   .tickSize(2)
   .tickPadding(5)
-  .tickFormat((d, i) => sampleData[i].city)
+  .tickFormat((d, i) => data[i].city)
 
-canvas.append('g')
+  canvas.append('g')
   .attr('transform', `translate(0, ${height-20})`)
   .call(cityAxis);
 
-// y-axis
-const growthScale = d3.scaleLinear()
+  // y-axis
+  const growthScale = d3.scaleLinear()
   .domain([growthMax, growthMin])
   .range([50, height-50]);
 
-const growthAxis = d3.axisLeft(growthScale)
-  .ticks(12)
+  const growthAxis = d3.axisLeft(growthScale)
+  .ticks(getTickNumber(growthMin, growthMax))
   .tickSize(2)
   .tickPadding(5);
 
-canvas.append('g')
+  canvas.append('g')
   .attr('transform', `translate(20, 0)`)
   .call(growthAxis);
 
-// Grid lines
-canvas.append('g')
+  // Making grid lines
+  canvas.append('g')
   .attr('class', 'grid')
   // .attr('transform', `translate(0, 0`)
   .call(d3.axisLeft()
-          .scale(growthScale)
-          .tickSize(-width, 0, 0)
-          .tickFormat(''));
+  .scale(growthScale)
+  .tickSize(-width, 0, 0)
+  .tickFormat(''));
 
-// Labels
+  return [data, cityScale, growthScale];
+
+}
+
+async function updateGraph() {
+  const [data, cityScale, growthScale] = await makeAxis();
+
+  // Binding and making shapes
+  const circles = canvas.selectAll('.circle')
+  .data(data);
+
+  circles.enter()
+  .append('circle')
+  .attr('class', 'circle')
+  .attr('cx', (d, i) => cityScale(1+i)) //So circles are made from 1 instead of 0
+  .attr('cy', (d, i) => growthScale(parseFloat(d.growth_from_2000_to_2013)))
+  .attr('r', (d, i) => setRadius(d.population));
+
+}
+
+updateGraph();
+
+
+// Setting amount of ticks
+function getTickNumber(min, max) {
+  return Math.ceil((max-min)/5);
+}
 
 // Setting radius based on population
 function setRadius(population) {
   const num = parseFloat(population);
-  const divider = 125000;
-  return (num < 625000) ? 5 : num / divider;
+  const divider = 50000;
+
+  if (num < 250000) return 5;
+  return (num > 1000000) ? 20 : num / divider;
 }
 
 
-// Binding and making shapes (sample only)
-const circles = canvas.selectAll('.circle')
-  .data(sampleData);
+// const [growthMin, growthMax] = growthRange(sampleData);
 
-circles.enter()
-  .append('circle')
-  .attr('class', 'circle')
-  .attr('cx', (d, i) => cityScale(1+i)) //So circles are made from 1 instead of 0
-  .attr('cy', (d, i) => growthScale(parseFloat(d.growth)))
-  .attr('r', (d, i) => setRadius(d.population));
+// x-axis
+// const cityScale = d3.scaleLinear()
+//   .domain([1, 10])
+//   .range([50, width-50]);
+//
+// const cityAxis = d3.axisBottom(cityScale)
+//   .ticks(10)
+//   .tickSize(2)
+//   .tickPadding(5)
+//   .tickFormat((d, i) => sampleData[i].city)
+
+// canvas.append('g')
+//   .attr('transform', `translate(0, ${height-20})`)
+//   .call(cityAxis);
+
+// y-axis
+// const growthScale = d3.scaleLinear()
+// .domain([growthMax, growthMin])
+// .range([50, height-50]);
+//
+// const growthAxis = d3.axisLeft(growthScale)
+//   .ticks(12)
+//   .tickSize(2)
+//   .tickPadding(5);
+
+// canvas.append('g')
+// .attr('transform', `translate(20, 0)`)
+// .call(growthAxis);
+
+
+// Grid lines
+// canvas.append('g')
+//   .attr('class', 'grid')
+//   // .attr('transform', `translate(0, 0`)
+//   .call(d3.axisLeft()
+//           .scale(growthScale)
+//           .tickSize(-width, 0, 0)
+//           .tickFormat(''));
+
+// Labels
+
+// Setting radius based on population
+// function setRadius(population) {
+//   const num = parseFloat(population);
+//   const divider = 125000;
+//   return (num < 625000) ? 5 : num / divider;
+// }
+//
+//
+// // Binding and making shapes (sample only)
+// const circles = canvas.selectAll('.circle')
+//   .data(sampleData);
+//
+// circles.enter()
+//   .append('circle')
+//   .attr('class', 'circle')
+//   .attr('cx', (d, i) => cityScale(1+i)) //So circles are made from 1 instead of 0
+//   .attr('cy', (d, i) => growthScale(parseFloat(d.growth_from_2000_to_2013)))
+//   .attr('r', (d, i) => setRadius(d.population));
